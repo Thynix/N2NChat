cbf532f606ad6224bd84574f632dbbe75183f4cd
Steve Dougherty <steve@asksteved.com>
2011-07-03 18:06:39 -0400

    Fix send invite button doing nothing due to mix-up between button name and value. Fix localization through restructuring. Fix message type not being properly parsed. Fix NPE when attempting to accept or reject a nonexistant invite. Fix date parsing problem that was preventing messages from being received.

---
+++ src/main/java/plugins/N2NChat/MainPageToadlet.java
@@ -0,0 +1,151 @@
+package plugins.N2NChat;
+
+import freenet.clients.http.*;
+import freenet.l10n.NodeL10n;
+import freenet.l10n.PluginL10n;
+import freenet.node.DarknetPeerNode;
+import freenet.node.Node;
+import freenet.node.NodeClientCore;
+import freenet.pluginmanager.PluginRespirator;
+import freenet.support.HTMLNode;
+import freenet.support.MultiValueTable;
+import freenet.support.api.HTTPRequest;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+
+
+/**
+ * Lists the chat rooms the node is currently participating in or has been invited to. Allows creation of new chat
+ * rooms and accepting or rejecting invitations received from others.
+ */
+public class MainPageToadlet extends Toadlet implements LinkEnabledCallback {
+
+	//TODO: Do we really have to take all this stuff from the chatPlugin? Seems excessive...
+	private Node node;
+	private NodeClientCore clientCore;
+	private PluginRespirator pluginRespirator;
+	private HashMap<Long, ChatRoom> chatRooms;
+	private HashMap<Long, N2NChatPlugin.chatInvite> receivedInvites;
+	//TODO: Or node l10n?
+	private PluginL10n l10n;
+	public static final int roomNameTruncate = 255;
+
+	public MainPageToadlet(N2NChatPlugin chatPlugin) {
+		super(chatPlugin.pluginRespirator().getHLSimpleClient());
+		this.clientCore = chatPlugin.pluginRespirator().getNode().clientCore;
+		this.pluginRespirator = chatPlugin.pluginRespirator();
+		this.node = chatPlugin.pluginRespirator().getNode();
+		this.l10n = chatPlugin.l10n();
+		this.chatRooms = chatPlugin.chatRooms;
+		this.receivedInvites = chatPlugin.receivedInvites;
+	}
+
+	public String path() {
+		return "/n2n-chat/main-page/";
+	}
+
+	public boolean isEnabled (ToadletContext ctx) {
+		return (!pluginRespirator.getToadletContainer().publicGatewayMode()) ||
+		        ((ctx != null) && ctx.isAllowedFullAccess());
+	}
+
+	public void handleMethodPOST(URI uri, HTTPRequest request, ToadletContext ctx) throws
+	        ToadletContextClosedException, IOException, RedirectException {
+
+		//TODO: This is duplicate code between here and DisplayChatToadlet; refactor.
+		//Ensure that the form password is correctly set.
+		String pass = request.getPartAsStringFailsafe("formPassword", 32);
+		if ( pass == null || !pass.equals(clientCore.formPassword)) {
+			MultiValueTable<String, String> headers = new MultiValueTable<String, String>();
+			headers.put("Location", path());
+			ctx.sendReplyHeaders(302, "Found", headers, null, 0);
+			return;
+		}
+
+		//TODO: Tell user about the maximum length of the room name instead of silently truncating.
+		if (request.isPartSet("new-room-name") &&
+			        !request.getPartAsStringFailsafe("new-room-name", roomNameTruncate).isEmpty()) {
+			long globalIdentifier = node.random.nextLong();
+			String roomName = request.getPartAsStringFailsafe("new-room-name", roomNameTruncate);
+			String userName = node.getMyName();
+			DarknetPeerNode[] darknetPeerNodes = node.getDarknetConnections();
+			chatRooms.put(globalIdentifier,
+			        new ChatRoom(roomName, globalIdentifier, userName, darknetPeerNodes, l10n));
+		}
+		handleMethodGET(uri, request, ctx);
+	}
+
+	public void handleMethodGET(URI uri, HTTPRequest request, ToadletContext ctx) throws
+	        ToadletContextClosedException, IOException, RedirectException {
+
+		if (!ctx.isAllowedFullAccess()) {
+			super.sendErrorPage(ctx, 403, "Unauthorized", NodeL10n.getBase()
+			        .getString("Toadlet.unauthorized"));
+			return;
+		}
+
+		if (request.isParameterSet("accept")) {
+			//User accepted an invite.
+			long globalIdentifier = Long.parseLong(request.getParam("accept"));
+			if (!receivedInvites.containsKey(globalIdentifier)) {
+				super.sendErrorPage(ctx, 403, l10n("invalidInviteTitle"), l10n("invalidInvite"));
+				return;
+			}
+			N2NChatPlugin.chatInvite invite = receivedInvites.get(globalIdentifier);
+			//Send invite acceptance.
+			N2NChatPlugin.sendInviteAccept(invite.darkPeer, globalIdentifier);
+			ChatRoom chatRoom = new ChatRoom(invite.roomName, globalIdentifier, invite.username,
+			        pluginRespirator.getNode().getDarknetConnections(), l10n, invite.darkPeer);
+			//Add the room to the list of rooms.
+			chatRooms.put(globalIdentifier, chatRoom);
+			//Invite is accepted and so no longer pending.
+			receivedInvites.remove(globalIdentifier);
+		} else if (request.isParameterSet("reject")) {
+			//User rejected an invite.
+			long globalIdentifier = Long.parseLong(request.getParam("reject"));
+			if (!receivedInvites.containsKey(globalIdentifier)) {
+				super.sendErrorPage(ctx, 403, l10n("invalidInviteTitle"), l10n("invalidInvite"));
+				return;
+			}
+			//Send the inviting peer the invite rejection.
+			N2NChatPlugin.sendInviteReject(receivedInvites.get(globalIdentifier).darkPeer, globalIdentifier);
+			//Invite is rejected and so no longer pending.
+			receivedInvites.remove(globalIdentifier);
+		}
+
+		//TODO: Localization
+		//List current chat rooms
+		PageNode pn = ctx.getPageMaker().getPageNode("Chat Room Listing", ctx);
+		HTMLNode content = pn.content;
+		HTMLNode roomListing = content.addChild("div", "class", "room-listing");
+		for (ChatRoom chatRoom : chatRooms.values()) {
+			roomListing.addChild("a", "href", DisplayChatToadlet.PATH+"?room="+chatRoom.getGlobalIdentifier(),
+			        chatRoom.getRoomName());
+		}
+		//Allow creating a new room
+		HTMLNode createChatForm = ctx.addFormChild(roomListing, path(), "create-room");
+		createChatForm.addChild("input", new String[] {"type", "name", "value"},
+		        new String[] { "text", "new-room-name", "New room name"});
+		createChatForm.addChild("input", new String[] { "type", "name", "value"},
+		        new String[] { "submit", "create-chat", l10n("newRoom") });
+
+		//List received invitations.
+		HTMLNode inviteListing = content.addChild("div", "class", "invite-listing");
+		for (long globalIdentifier : receivedInvites.keySet()) {
+			N2NChatPlugin.chatInvite invite = receivedInvites.get(globalIdentifier);
+			HTMLNode entry = inviteListing.addChild("p");
+			entry.addChild("#", "Room Name: "+invite.roomName+" Username: "+invite.username+" Invited by:"
+			        +invite.darkPeer.getName());
+			entry.addChild("a", "href", path()+"?accept="+globalIdentifier, l10n("accept") );
+			entry.addChild("a", "href", path()+"?reject="+globalIdentifier, l10n("reject") );
+		}
+
+		writeHTMLReply(ctx, 200, "OK", null, pn.outer.generate());
+	}
+
+	private String l10n(String key) {
+		return l10n.getBase().getString("main."+key);
+	}
+}
---
+++ src/main/java/plugins/N2NChat/N2NChatPlugin.java
@@ -0,0 +1,396 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package plugins.N2NChat;
+
+import freenet.l10n.BaseL10n.LANGUAGE;
+import freenet.l10n.NodeL10n;
+import freenet.l10n.PluginL10n;
+import freenet.node.DarknetPeerNode;
+import freenet.node.FSParseException;
+import freenet.node.NodeToNodeMessageListener;
+import freenet.node.PeerNode;
+import freenet.pluginmanager.*;
+import freenet.support.Base64;
+import freenet.support.IllegalBase64Exception;
+import freenet.support.SimpleFieldSet;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.logging.Logger;
+
+/**
+ * This class interfaces with Freenet. It is the class that is loaded by the node.
+ */
+public class N2NChatPlugin implements FredPlugin, FredPluginL10n, FredPluginBaseL10n, FredPluginThreadless, FredPluginVersioned {
+
+	//
+	// CONSTANTS
+	//
+
+	/** Type identifier for all chat messages. Used to register message listener.*/
+	public static final int N2N_MESSAGE_TYPE_CHAT = 103;
+
+	/** Type identifier for a message */
+	public static final int MESSAGE = 1;
+
+	/** Type identifier for an invite offer */
+	public static final int OFFER_INVITE = 2;
+
+	/** Type identifier for a retraction of an invite */
+	public static final int RETRACT_INVITE = 3;
+
+	/** Type identifier for accepting an invite */
+	public static final int ACCEPT_INVITE = 4;
+
+	/** Type identifier for rejecting an invite */
+	public static final int REJECT_INVITE = 5;
+
+	/** Type identifier for a participant joining */
+	public static final int JOIN = 6;
+
+	/** Type identifier for a participant leaving */
+	public static final int LEAVE = 7;
+
+	/** The version. */
+	public static final String VERSION = "0.0.1";
+
+	//TODO: Allow date display formatting configuration.
+	/** Date format used for timestamp when a message is received on a different day than the last.
+	 * Ex: Wednesday, June 1, 2011
+	 **/
+	public static final SimpleDateFormat dayChangeFormat = new SimpleDateFormat("EEEE, MMMM dd, yyyy");
+
+	/** Date format used for the "time received" timestamp on messages. Ex: 04:48:30 PM */
+	public static final SimpleDateFormat messageReceivedFormat = new SimpleDateFormat("hh:mm:ss a");
+
+	/** Date format used for the "time composed" timestamp on messages. Ex: 04:48:30 PM, June 1, 2011*/
+	public static final SimpleDateFormat messageComposedFormat = new SimpleDateFormat("hh:mm:ss a, MMMM dd, yyyy");
+
+	//
+	// MEMBER VARIABLES
+	//
+
+	/** The logger. */
+	//private static final Logger logger = Logging.getLogger(N2NChatPlugin.class);
+
+	/** The plugin respirator. */
+	private PluginRespirator pluginRespirator;
+
+	/** The l10n helper. */
+	private PluginL10n l10n;
+
+	/** HashMap containing all chat rooms this node is present in. The key is the global identifier. */
+	public HashMap<Long, ChatRoom> chatRooms;
+
+	/** Key is global identifier*/
+	public HashMap<Long, chatInvite> receivedInvites;
+
+	private MainPageToadlet mpt;
+	private DisplayChatToadlet displayChatToadlet;
+
+	//
+	// ACCESSORS
+	//
+
+	/**
+	 * Returns the plugin respirator for this plugin.
+	 *
+	 * @return The plugin respirator
+	 */
+	public PluginRespirator pluginRespirator() {
+		return pluginRespirator;
+	}
+
+	/**
+	 * Returns the plugin’s l10n helper.
+	 *
+	 * @return The plugin’s l10n helper
+	 */
+	public PluginL10n l10n() {
+		return l10n;
+	}
+
+	//
+	// FREDPLUGIN METHODS
+	//
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void runPlugin(PluginRespirator pluginRespirator) {
+		this.pluginRespirator = pluginRespirator;
+		this.chatRooms = new HashMap<Long, ChatRoom>();
+		this.receivedInvites = new HashMap<Long, chatInvite>();
+
+		//TODO: Need to store and retrieve config somehow.
+		mpt = new MainPageToadlet(this);
+		//pluginRespirator.getToadletContainer().register(mpt,
+		//         "FProxyToadlet.categoryFriends", mpt.path(), true, "N2NChatRoom.chatName", "", true, mpt);
+		pluginRespirator.getToadletContainer().register(mpt, null, mpt.path(), true, false);
+
+		displayChatToadlet = new DisplayChatToadlet(this);
+		pluginRespirator.getToadletContainer().register(displayChatToadlet, null, displayChatToadlet.path(), true, false);
+		pluginRespirator.getNode().registerNodeToNodeMessageListener(N2N_MESSAGE_TYPE_CHAT, N2NChatListener);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void terminate() {
+		//Disconnect from all chats
+		for (ChatRoom chatRoom : chatRooms.values()) {
+			chatRoom.disconnect();
+		}
+		//Unregister pages
+		pluginRespirator.getToadletContainer().unregister(mpt);
+		pluginRespirator.getToadletContainer().unregister(displayChatToadlet);
+	}
+
+	//
+	// INTERFACE FredPluginL10n
+	//
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getString(String key) {
+		return l10n.getBase().getString(key);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void setLanguage(LANGUAGE newLanguage) {
+		l10n = new PluginL10n(this, newLanguage);
+	}
+
+	//
+	// INTERFACE FredPluginBaseL10n
+	//
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getL10nFilesBasePath() {
+		return "l10n/";
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getL10nFilesMask() {
+		return "N2NChat.${lang}.properties";
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getL10nOverrideFilesMask() {
+		return "N2NChat.${lang}.override.properties";
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public ClassLoader getPluginClassLoader() {
+		return N2NChatPlugin.class.getClassLoader();
+	}
+
+	//
+	// INTERFACE FredPluginVersioned
+	//
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getVersion() {
+		return VERSION;
+	}
+
+	public static void sendInvite(long globalIdentifier, DarknetPeerNode darkPeer, int type) {
+		SimpleFieldSet fs = new SimpleFieldSet(true);
+		fs.put("type", type);
+		fs.put("globalIdentifier", globalIdentifier);
+		darkPeer.sendNodeToNodeMessage(fs, N2N_MESSAGE_TYPE_CHAT, true, System.currentTimeMillis(), false);
+	}
+
+
+	public static void sendInviteAccept(DarknetPeerNode darkPeer, long globalIdentifier) {
+		System.out.println("Sent invite accept for room " + globalIdentifier + " to " + darkPeer.getName());
+		sendInvite(globalIdentifier, darkPeer, ACCEPT_INVITE);
+	}
+
+	public static void sendInviteReject(DarknetPeerNode darkPeer, long globalIdentifier) {
+		System.out.println("Sent invite reject for room "+globalIdentifier+" to "+darkPeer.getName());
+		sendInvite(globalIdentifier, darkPeer, REJECT_INVITE);
+	}
+
+	public class chatInvite {
+		public final String username;
+		public final String roomName;
+		public final DarknetPeerNode darkPeer;
+
+		chatInvite(String username, String roomName, DarknetPeerNode darkPeer) {
+			this.username = username;
+			this.roomName = roomName;
+			this.darkPeer = darkPeer;
+		}
+	}
+
+	private NodeToNodeMessageListener N2NChatListener = new NodeToNodeMessageListener() {
+		public void handleMessage(byte[] data, boolean fromDarknet, PeerNode source, int type) {
+			if (!fromDarknet) {
+				freenet.support.Logger.error(this, "Received N2N chat message from non-darknet peer " + source);
+				return;
+			}
+			DarknetPeerNode darkSource = (DarknetPeerNode) source;
+			freenet.support.Logger.normal(this, "Received N2N chat from '" + darkSource.getPeer() + "'");
+			SimpleFieldSet fs = null;
+			try {
+				fs = new SimpleFieldSet(new String(data, "UTF-8"), false, true);
+			} catch (UnsupportedEncodingException e) {
+				throw new Error("Impossible: JVM doesn't support UTF-8: " + e, e);
+			} catch (IOException e) {
+				freenet.support.Logger.error(this, "IOException while parsing node to node message data", e);
+				return;
+			}
+
+			/*Fields are parsed in the order that they are needed, with those types that need the fewest
+			pieces of information from fields checked for first.
+			 */
+
+			//Get global identifier.
+			long globalIdentifier;
+			try {
+				globalIdentifier = fs.getLong("globalIdentifier");
+			} catch (FSParseException e) {
+				//Could not parse global identifier. Dropping.
+				//TODO: Add localized, logged error message.
+				System.out.println("Failed to parse global identifier from "+((DarknetPeerNode) source).getName()+'.');
+				return;
+			}
+
+			//Now we don't care that it was a chat message, but what kind.
+			try {
+				type = fs.getInt("type");
+			} catch (FSParseException e) {
+				System.out.println("Failed to read message type in message about room "+globalIdentifier);
+			}
+
+			/*/A darknet peer offered this node an invite. Add it to the list of offered invites to allow
+			the user to accept or reject it. If there is an existing invite for this room, it is replaced.
+			 */
+			if (type == OFFER_INVITE) {
+				try {
+					String username = new String(Base64.decode(fs.get("username")));
+					String roomName = new String(Base64.decode(fs.get("roomName")));
+					receivedInvites.put(globalIdentifier, new chatInvite(username, roomName,
+					       darkSource));
+				} catch (IllegalBase64Exception e) {
+					freenet.support.Logger.error(this, "Invalid base64 encoding on user/room name", e);
+				}
+				System.out.println("Received invite to room "+globalIdentifier+" from "+darkSource.getName());
+				return;
+			} else if (type == RETRACT_INVITE) {
+				if (receivedInvites.containsKey(globalIdentifier) &&
+				        receivedInvites.get(globalIdentifier).darkPeer == darkSource) {
+					receivedInvites.remove(darkSource.getPubKeyHash());
+				}
+				System.out.println("Received invite retract for room "+globalIdentifier+" from "+darkSource.getName());
+				return;
+			}
+
+			//Check that the requested room exists.
+			if (!chatRooms.containsKey(globalIdentifier)) {
+				freenet.support.Logger.error(this, l10n.getBase().getString("plugin.nonexistentRoom",
+				        new String[] { "globalIdentifier", "type" },
+				        new String[] { String.valueOf(globalIdentifier), String.valueOf(type) }));
+				return;
+			}
+
+			//TODO: Do these need to fire web pushing events?
+
+			//A darknet peer accepted an invite this node offered. Add them to the chat room.
+			if (type == ACCEPT_INVITE) {
+				chatRooms.get(globalIdentifier).receiveInviteAccept(darkSource);
+				System.out.println("Received invite accept to room "+globalIdentifier+" from "+darkSource.getName());
+				return;
+			//A darknet peer rejected an invite this node offered; remove it from list of pending invites.
+			} else if (type == REJECT_INVITE) {
+				chatRooms.get(globalIdentifier).receiveInviteReject(darkSource);
+				System.out.println("Received invite reject to room "+globalIdentifier+" from "+darkSource.getName());
+				return;
+			}
+
+			//Get identity hash for use in a message, join, or leave.
+			byte[] pubKeyHash;
+			try {
+				pubKeyHash = Base64.decode(fs.getString("pubKeyHash"));
+			} catch (FSParseException e) {
+				//pubKeyHash was not included. This means it pertains to the sender.
+				pubKeyHash = darkSource.getPubKeyHash();
+			} catch (IllegalBase64Exception e) {
+				//Could not parse identity hash. Dropping.
+				//TODO: Add localized, logged error message.
+				System.out.println("Failed to parse public key hash from "+darkSource.getName()+'.');
+				return;
+			}
+
+			//A message was received. Attempt to add the message.
+			if (type == MESSAGE) {
+				try {
+					chatRooms.get(globalIdentifier).receiveMessage(
+					        pubKeyHash,
+					        new Date(fs.getLong("timeComposed")),
+					        darkSource.getPubKeyHash(), new String(Base64.decode(fs.get("text"))));
+				} catch (FSParseException e) {
+					//TODO: Add localized, logged error message.
+					System.out.println("Failed to parse date from "+darkSource.getName()+'.');
+				} catch (IllegalBase64Exception e) {
+					freenet.support.Logger.error(this, "Invalid base64 encoding on message text", e);
+				}
+				System.out.println("Received text message in room "+globalIdentifier+" from "+darkSource.getName());
+			//Someone joined a chat room.
+			} else if (type == JOIN) {
+				try {
+					chatRooms.get(globalIdentifier).joinedParticipant(pubKeyHash,
+ 					        new String(Base64.decode(fs.get("username"))), darkSource);
+				} catch (IllegalBase64Exception e) {
+					freenet.support.Logger.error(this, "Invalid base64 encoding on username", e);
+				}
+				System.out.println("Received join in room "+globalIdentifier+" from "+darkSource.getName());
+			//Someone left a chat room.
+			} else if (type == LEAVE) {
+				chatRooms.get(globalIdentifier).removeParticipant(pubKeyHash,
+				        darkSource.getPubKeyHash(), false);
+				System.out.println("Received leave in room "+globalIdentifier+" from "+darkSource.getName());
+			}
+		}
+	};
+}
