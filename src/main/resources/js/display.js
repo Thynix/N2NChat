69c5243d53609249fb86654af6a1577f77d4e61f
Steve Dougherty <steve@asksteved.com>
2011-07-16 23:00:29 -0400

    Initial support for remaining scrolled to the bottom when new messages appear.

--- src/main/resources/js/display.js
+++ src/main/resources/js/display.js
@@ -1,30 +1,9 @@
-//Values needed for any room that will not change for that room.
-var room;
-var formPassword;
-
-function refreshPanes() {
-  //TODO: Scroll down to new bottom if user is currently at the bottom, otherwise restore the position.
-  refreshMessagePane();
-  refreshParticipantsList();
-  setTimeout(refreshPanes, 1000);
-}
-
-function refreshParticipantsList() {
-  $.get('/n2n-chat/display/', { 'room' : room, 'participantsList' : 'only' }, function(data) {
-    $('#participants-list').html(data);
-  });
-}
-
-function refreshMessagePane() {
-  $.get('/n2n-chat/display/', { 'room' : room, 'messagesPane' : 'only' }, function(data) {
-    $('#messages-pane').html(data);
-  });
-}
-
 $(document).ready(function() {
-  room = $('[name="room"]').val();
-  formPassword = $('[name="formPassword"]').val();
+  var room = $('[name="room"]').val();
+  var formPassword = $('[name="formPassword"]').val();
   var element = $('[name="message"]');
+  var msgPane = $('#messages-pane');
+
   element.keydown(function(event) {
     if (event.which == 13) {
       //As this is being POSTed manually, suppress the form submitting again, which would submit twice.
@@ -38,5 +17,38 @@ $(document).ready(function() {
       });
      }
     });
+
+  function refreshParticipantsList() {
+    $.get('/n2n-chat/display/', { 'room' : room, 'participantsList' : 'only' }, function(data) {
+      $('#participants-list').html(data);
+    });
+  }
+
+  function refreshMessagePane() {
+    //If at the bottom, load new content, then scroll down to the new bottom.
+    if (atBottom()) {
+      $.get('/n2n-chat/display/', { 'room' : room, 'messagesPane' : 'only' }, function(data) {
+        msgPane.html(data);
+        msgPane.animate({scrollTop: msgPane[0].scrollHeight});
+      });
+    } else {
+       $.get('/n2n-chat/display/', { 'room' : room, 'messagesPane' : 'only' }, function(data) {
+        msgPane.html(data);
+      });
+    }
+  }
+
+  function atBottom() {
+      //Total height of scrollable element - visible height - height scrollable area hidden above.
+      //If this is less than 0,  it's scrolled down to the bottom line.
+      return msgPane[0].scrollHeight - msgPane.height() - msgPane.scrollTop() < 0;
+  }
+
+  function refreshPanes() {
+    refreshMessagePane();
+    refreshParticipantsList();
+    setTimeout(refreshPanes, 1000);
+  }
+
   refreshPanes();
 });
